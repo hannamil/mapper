43e91b560a091a15e94d62170bb80bdeb7d525f7
chris han <namilhan@gmail.com>
2021-01-03 11:30:19 +0900

    Fix struct compare bug

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# mapper
+# hannamil/mapper
+---
+A simple go `struct` mapper.
+### 1. Install
+```bash
+go get -u github.com/hannamil/mapper
+```
+
+### 2. Getting Started
+
+```go
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hannamil/mapper"
+)
+
+type (
+	Class struct {
+		Name  string
+		Level string
+	}
+	
+	Person struct {
+		Name  string
+		Age   int
+		Birth time.Time
+		Class Class
+	}
+
+	Student struct {
+		Name  string
+		Age   int
+		Class interface{}
+	}
+)
+
+func main() {
+	person := &Person{
+		Name: "Chris han",
+		Age:  40,
+		Class: Class{
+			Name: "algorithm",
+			Level: "basic",
+        },
+	}
+	student, err := mapper.Mapper(person, &Student{})
+	// or
+	// student, err := mapper.Mapper(person, Student{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("student: %v\n", student)
+}
+```
+### 3. output
+```bash
+student: {Chris han 40 {algorithm basic}}
+```
--- mapper_test.go
+++ mapper_test.go
@@ -1,16 +1,47 @@
 package mapper
 
 import (
+	"reflect"
 	"testing"
+	"time"
 )
-type Source struct {
-	Idx int64
-	Name string
+type (
+	Class struct {
+		Name  string
+		Level string
+	}
+
+	Person struct {
+		Id    string
+		Name  string
+		Age   int
+		Birth time.Time
+		Class Class
+	}
+
+	Student struct {
+		No    int
+		Name  string
+		Age   int
+		Class interface{}
+	}
+)
+
+func BenchmarkMapper(b *testing.B) {
+	tests := []struct {
+		source interface{}
+		dest interface{}
+	}{
+		{source: nil, dest: nil},
+		{source: Person{}, dest: nil},
+		{source: Person{Id: "nih", Name: "chris han", Age: 30, Class: Class{Name: "Deep learning", Level: "basic"}}, dest: Student{}},
+		{source: Person{Name: "chris han", Age: 30, Birth: time.Now().Add(time.Hour*24*365*30*-1)}, dest: Student{}},
+	}
+	for _, tt := range tests {
+		_, _ = Mapper(tt.source, tt.dest)
+	}
 }
 
-type Dest struct {
-	Idx int64
-}
 func TestMapper(t *testing.T) {
 	type args struct {
 		source interface{}
@@ -22,7 +53,12 @@ func TestMapper(t *testing.T) {
 		want    interface{}
 		wantErr bool
 	}{
-		{name: "", args: args{ source: Source{Idx: 1, Name: "namil"}, dest: Dest{}}, want: Dest{Idx: 1}, wantErr: false},
+		{name: "Zero value source", args: args{ source: nil, dest: Student{}}, want: nil, wantErr: true},
+		{name: "Zero value dest", args: args{ source: Person{}, dest: nil}, want: nil, wantErr: true},
+		{name: "Zero value source/dest", args: args{ source: nil, dest: nil}, want: nil, wantErr: true},
+		{name: "Default value source", args: args{ source: Person{}, dest: Student{}}, want: Student{Class: Class{}}, wantErr: false},
+		{name: "Default value dest", args: args{ source: Person{Name: "chris han", Age: 30, Birth: time.Now().Add(time.Hour*24*365*30*-1)}, dest: Student{}}, want: Student{Name: "chris han", Age: 30, Class: Class{}}, wantErr: false},
+		{name: "Normal", args: args{ source: Person{Id: "nih", Name: "chris han", Age: 30, Class: Class{Name: "Deep learning", Level: "basic"}}, dest: Student{}}, want: Student{Name: "chris han", Age: 30, Class: Class{Name: "Deep learning", Level: "basic"}}, wantErr: false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -31,9 +67,13 @@ func TestMapper(t *testing.T) {
 				t.Errorf("Mapper() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got == tt.want {
-				t.Errorf("Mapper() got = %v, want %v", got, tt.want)
+			if err == nil && !IsEqual(got, tt.want) {
+				t.Errorf("Mapper() got = %v, want = %v", got, tt.want)
 			}
 		})
 	}
+}
+
+func IsEqual(dest, want interface{}) bool {
+	return reflect.DeepEqual(dest.(reflect.Value).Interface().(Student), want)
 }
